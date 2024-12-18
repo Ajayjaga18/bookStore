f09c354cd429b15f07fb1c1934efb0023d621313
Ajay Jaga <ajayjaga1111@gmail.com>
2024-12-18 15:09:07 +0530

    add bannner file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/banner'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-   
+      <Navbar />
+    <Banner/>
     </div>
   )
 }

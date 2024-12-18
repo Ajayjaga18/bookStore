2e07e56550bb9bfb1272ddabbcac92f2bc124093
Ajay Jaga <ajayjaga1111@gmail.com>
2024-12-18 15:42:46 +0530

    add footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/banner'
+import Navbar from './components/Navbar';
+import Banner from './components/banner';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Navbar />
     <Banner/>
+    <Footer/>
     </div>
   )
 }
